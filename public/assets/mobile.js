a8394be39266b7a211aeca8d7380f5de20dc7d84
charlinehubaux <charline.hubaux@etu.iad-arts.be>
2021-06-19 11:46:51 +0200

    Working

--- public/assets/mobile.js
+++ public/assets/mobile.js
@@ -1,9 +1,9 @@
 
 
 //Variable Socket pour les commandes socket.io
-//const URL = "http://localhost";
+const URL = "http://localhost";
 
-const URL = "192.168.1.44:5000";
+//const URL = "192.168.1.44:5000";
 const socket = io(URL, { autoConnect: false });
 
 
